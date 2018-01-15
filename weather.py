161634d3f089e73eb9bcbe42a6929ce69d13c03f
Atharva Lele <itsatharva@gmail.com>
2018-01-15 22:56:04 +0530

    7 day forecast, basic exception handling

--- weather.py
+++ weather.py
@@ -4,13 +4,18 @@
 import pyowm
 import string   # for capwords()
 
-# API Keys
-owm = pyowm.OWM('b7686ed6ad45471e6ed20e35f9f860c1')
+# API Key
+api_key = # your api key
+owm = pyowm.OWM(api_key)
 
 # Setting current location
 print('\nEnter location: ', end='')
 location = input()
-current_location = owm.weather_at_place(location)
+try:
+        current_location = owm.weather_at_place(location)
+except:
+        print("Cannot find location")
+        exit()
 w = current_location.get_weather()
 
 # Weather Details
@@ -24,4 +29,10 @@ print(string.capwords(w.get_detailed_status()))
 print('\nCurrent: ' + str(temps['temp']) + 'C' + '\nMax: ' + str(temps['temp_max']) + 'C' +
         '\nMin: ' + str(temps['temp_min']) + 'C')
 print('\nWind: ' + str((wind['speed'] * 3.6)) + ' km/h')
-print('\nHumidity: ' + str(humidity) + '%\n')
+print('Humidity: ' + str(humidity) + '%\n')
+
+# 7 day forecast
+fc = owm.daily_forecast(location, limit = 7)
+f = fc.get_forecast()
+for day in f:
+        print(day.get_reference_time('iso')[:10] + ' ' + string.capwords(day.get_detailed_status()))
